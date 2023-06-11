823cd58bd7d3dd88b00942e510f75e7108e644dc
MounaGuelsa <mounaguelsa9991@gmail.com>
2023-06-11 18:56:22 +0100

    test

--- src/main/java/com/example/project1/repository/ClientRepository.java
+++ src/main/java/com/example/project1/repository/ClientRepository.java
@@ -2,10 +2,11 @@ package com.example.project1.repository;
 
 import com.example.project1.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+
+
     }
